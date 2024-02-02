6a01290396722ab3ea050ba69674d4408471bd2b
Raza Quadir <raza.quadir@oxfordcc.co.uk>
2024-02-02 12:48:53 +0000

    #3 - cwe import - tidied up icons

---
+++ bundles/dsm.cwe.model.edit/src-man/dsm/cwe/model/edit/ExternalReferencesItemProviderCustomImpl.java
@@ -0,0 +1,29 @@
+/******************************************************************************************************
+* Copyright Israel Aerospace Industries, Eclipse contributors and others 2021. All rights reserved.
+* This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License 2.0
+* which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-2.0/
+* 
+* SPDX-License-Identifier: EPL-2.0
+* 
+* Contributors:
+*     ELTA Ltd - initial API and implementation
+* 
+*******************************************************************************************************/
+package dsm.cwe.model.edit;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import dsm.cwe.model.CWECatalog.provider.ExternalReferencesItemProvider;
+
+public class ExternalReferencesItemProviderCustomImpl extends ExternalReferencesItemProvider {
+
+	public ExternalReferencesItemProviderCustomImpl(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/ExternalReferences.png"));
+	}
+}
