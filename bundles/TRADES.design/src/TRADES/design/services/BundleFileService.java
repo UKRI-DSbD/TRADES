c2473b9eb88cdad6a631a58edebc64d14b650562
Neil Bevis <neil.bevis@systemc.com>
2025-03-12 09:42:44 +0000

    #15 - Export to Prolog - Minor update to comments

--- bundles/TRADES.design/src/TRADES/design/services/BundleFileService.java
+++ bundles/TRADES.design/src/TRADES/design/services/BundleFileService.java
@@ -11,11 +11,17 @@ import org.eclipse.core.runtime.Plugin;
 
 
 /**
- * Provides runtime access to bundle content
+ * Provides runtime access to bundle content. For example, the Prolog export prefix is a plain file in the source code, but at
+ * runtime it is bundled up inside a .jar file. This allows easy access to the contents of that file at runtime.
+ * 
+ * Additionally, this class handles conversion of line endings to the running system's format.
  * */
 public class BundleFileService extends Plugin {
 
-	/** Handles conversion of line endings the system's format */
+	/** 
+	 * Obtain the contents of the bundled file as a string (with line endings matching the running system)
+	 * @param filePath The bundled file path, e.g. "src/Resources/PrologExportPrefix.pl" for a file in this project's src/Resources directory
+	 * */
 	public String getResourceFileText(String filePath) {
 		try(InputStream stream = FileLocator.openStream(this.getBundle(), new Path(filePath), false);
 				InputStreamReader inputStreamReader = new InputStreamReader(stream);
